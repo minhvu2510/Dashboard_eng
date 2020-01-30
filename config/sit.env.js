b9ddae98ee12a96ba2a05da9876a6da1a78a3f20
minhvu <vunguyenminh@vccorp.vn>
2020-01-30 17:09:03 +0700

    mv

--- config/sit.env.js
+++ config/sit.env.js
@@ -6,6 +6,6 @@ module.exports = {
   BILLING: "'https://api-stagging-callcenter.vccloud.vn'",
   // TEST_LOCAL: "'http://209.97.172.91:8100'"
   // TEST_LOCAL: "'http://0.0.0.0:8100'"
-  TEST_LOCAL: "'http://10.5.20.34:8100'"
+  TEST_LOCAL: "'http://10.5.22.7:8100'"
   // TEST_LOCAL: "'209.97.172.91:8100'"
 }
