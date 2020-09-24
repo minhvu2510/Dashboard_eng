49a7683a210ca4b15db37a189ef68733b6698b73
minhvu <vunguyenminh@vccorp.vn>
2020-09-24 11:57:46 +0700

    Thêm fe

---
+++ fe_eng/src/api/department.js
@@ -0,0 +1,60 @@
+import request from '@/utils/request'
+
+export function addExtension(data) {
+  return request({
+    url: '/extension',
+    method: 'post',
+    data
+  })
+}
+export function addNote(data) {
+  return request({
+    url: '/note',
+    method: 'post',
+    data
+  })
+}
+export function getExtention(params) {
+  return request({
+    url: '/extension',
+    method: 'get',
+    params
+  })
+}
+export function getConfuse(params) {
+  return request({
+    url: '/confuse',
+    method: 'get',
+    params
+  })
+}
+export function userInfor(params) {
+  return request({
+    url: '/user',
+    method: 'get',
+    params
+  })
+}
+export function getNote(params) {
+  return request({
+    url: '/note',
+    method: 'get',
+    params
+  })
+}
+export function updateDepartment(data) {
+  return request({
+    url: '/department',
+    method: 'put',
+    data
+  })
+}
+
+export function deleteDepartment(params) {
+  return request({
+    url: 'department',
+    method: 'delete',
+    data: params
+  })
+}
+
